731abc1baf5885a4750c6b5b37896795b653e55a
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-12 16:38:25 -0500

    Imported MenuData and generic Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { FaBars } from "react-icons/fa"
+import { menuData } from "../data/MenuData";
+import { Button } from "./Button";
 import styled from "styled-components"
 
 const Nav = styled.nav`
@@ -35,12 +37,40 @@ const Bars = styled(FaBars)`
     cursor: pointer;
   }
 `
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -40px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
 
 const Header = () => {
   return (
     <Nav>
       <NavLink to="/">Jordan W. Hunter</NavLink>
       <Bars />
+      <NavMenu>
+        {menuData.map((item, index) => (
+          <NavLink to={item.link} key={index}>
+            {item.title}
+          </NavLink>
+        ))}
+      </NavMenu>
+      <NavBtn>
+        <Button primary="true" round="true" to="/contact">Email Me</Button>
+      </NavBtn>
     </Nav>
   )
 }
