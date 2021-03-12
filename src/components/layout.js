abeae20f545d90713555e55f748723400955ff23
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-12 16:01:17 -0500

    Implemented Style Sheet in Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,11 @@
 import * as React from "react"
+import { GlobalStyle } from "./styles/GlobalStyles"
 import Header from "./Header"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <main>{children}</main>
     </>
