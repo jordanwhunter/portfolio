a665b5d4c3d04e7f554aa44b599fbad01173e3e1
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-12 16:43:03 -0500

    Imported Hero component into index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Hero from "../components/Hero"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <Hero />
   </Layout>
 )
 
