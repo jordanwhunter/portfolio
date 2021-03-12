f7ca6ddf09c3fe025d4867b970f6358fef04b8b5
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-12 15:57:07 -0500

    Structuring Header with styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,48 @@
 import * as React from "react"
+import { Link } from "gatsby"
+import { FaBars } from "react-icons/fa"
+import styled from "styled-components"
+
+const Nav = styled.nav`
+  background: red;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1300px) / 2);
+  z-index: 100;
+  position: relative;
+`
+const NavLink = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+`
+const Bars = styled(FaBars)`
+  display: none;
+  color: #fff;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`
 
 const Header = () => {
-  return <h1>Header Block</h1>
+  return (
+    <Nav>
+      <NavLink to="/">Jordan W. Hunter</NavLink>
+      <Bars />
+    </Nav>
+  )
 }
 
 export default Header
