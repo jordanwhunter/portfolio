b4a600e1f046bddc23fd83d1fb1a61918438ffcf
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-15 11:07:54 -0400

    Imported portfolio component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import * as React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/Hero"
+import Portfolio from "../components/Portfolio"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
+    <Portfolio />
   </Layout>
 )
 
