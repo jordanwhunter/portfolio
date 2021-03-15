96a963f226fd950baf59db109b7030f1b473ca84
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-15 10:36:27 -0400

    Added styled components for Video and HeroContent

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -15,13 +15,26 @@ const HeroContainer = styled.div`
   margin-top: -80px;
 `
 const HeroBG = styled.div`
-
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
 `
 const Video = styled.video`
-
+  width: 100%;
+  height: 100%; 
+  -o-object-fit: cover;
+  object-fit: cover;
 `
 const HeroContent = styled.div`
-
+  z-index: 3;
+  height: calc(100vh - 80px);
+  max-height: 100%;
+  padding: 0rem calc((100vh - 1300px) / 2);
 `
 const HeroItems = styled.div`
 
