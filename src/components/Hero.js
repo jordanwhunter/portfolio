2511e9164aba3d85f4cc1b422541df70abb55a32
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-15 10:57:11 -0400

    Added sstyled components for HeroItems, HeroH1, HeroP, and :before for HeroContainer

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,6 +13,25 @@ const HeroContainer = styled.div`
   padding: 0 1rem;
   position: relative;
   margin-top: -80px;
+
+  :before {
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    z-index: 2;
+    background: linear-gradient(
+      180deg, 
+      rgba(0, 0, 0, 0.2) 0%, 
+      rgba(0, 0, 0, 0.6) 100%
+    ), linear-gradient(
+      180deg, 
+      rgba(0, 0, 0, 0.2) 0%, 
+      transparent 100%
+    );
+  }
 `
 const HeroBG = styled.div`
   position: absolute;
@@ -37,13 +56,32 @@ const HeroContent = styled.div`
   padding: 0rem calc((100vh - 1300px) / 2);
 `
 const HeroItems = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100vh;
+  max-height: 100%;
+  padding: 0;
+  color: #fff;
+  line-height: 1.1;
+  font-weight: bold; 
 `
 const HeroH1 = styled.h1`
-
+  /* clamp essentially makes fonts responsive w/out adding media queries */
+  /* first value (1.5rem) is smallest font will be */
+  /* second value is viewport width between 2 - infinity */
+  /* third value is max size */
+  font-size: clamp(1.5rem, 6vw, 4rem);
+  margin-bottom: 1.5rem;
+  letter-spacing: 3px;
+  font-weight: bold;
+  padding: 0 1rem;
 `
 const HeroP = styled.p`
-
+  font-size: clamp(1rem, 3vw, 3rem);
+  margin-bottom: 2rem;
 `
 
 export default function Hero() {
@@ -67,7 +105,14 @@ export default function Hero() {
           <HeroP>
             Test Text
           </HeroP>
-          <Button>Test</Button>
+          <Button
+            primary="true"
+            big="true"
+            round="true"
+            to="/portfolio"
+          >
+            Test
+          </Button>
         </HeroItems>
       </HeroContent>
     </HeroContainer>
