4580f721380e9014c97e1f3ecaff2c4123590fef
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-16 05:15:31 -0400

    Updates

--- src/data/MenuData.js
+++
@@ -1,6 +0,0 @@
-export const menuData = [
-  {title: "Portfolio", link: "portfolio"},
-  {title: "About", link: "about"},
-  {title: "Socials", link: "socials"},
-  {title: "Newsletter", link: "newsletter"},
-]
