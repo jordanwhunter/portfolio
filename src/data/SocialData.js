1037b5ba28a7cecec6929448b28b3d1390bd8488
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-16 02:28:00 -0400

    Created array of objects containing SocialData key values

---
+++ src/data/SocialData.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { GrLinkedin, GrGithub, GrInstagram, GrFacebook } from "react-icons/gr"
+
+export const socialData = [
+  {
+    icon: (<GrGithub />),
+    title: "GitHub",
+    description: "Let's connect on GitHub!",
+    src: "https://github.com/jordanwhunter"
+  },
+  {
+    icon: (<GrLinkedin />),
+    title: "LinkedIn",
+    description: "Let's connect on LinkedIn!",
+    src: "https://www.linkedin.com/in/jordanhunterr/"
+  },
+  {
+    icon: (<GrFacebook />),
+    title: "Facebook",
+    description: "Let's connect on Facebook!",
+    src: "https://www.facebook.com/jordanwhunterr/"
+  },
+  {
+    icon: (<GrInstagram />),
+    title: "Instagram",
+    description: "Let's connect on Instagram!",
+    src: "https://www.instagram.com/jordanwhunter/"
+  }
+]
