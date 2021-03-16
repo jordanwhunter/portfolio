308c372c94e831aa1a539980f0e834eaaad61b86
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-16 04:59:55 -0400

    Built Email component

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,9 +1,111 @@
 import React from 'react'
+import styled from "styled-components"
+import emailbg from "../assets/images/email-bg.jpg"
+import { Button } from "./Button"
+
+const EmailContainer = styled.div`
+  background: linear-gradient(
+    180deg, 
+    rgba(0, 0, 0, 0.5) 0%,
+    rgba(0, 0, 0, 0.5) 35%,
+    rgba(0, 0, 0, 0.1) 100%
+  ),
+  url(${emailbg}) no-repeat center;
+  background-size: cover;
+  height: 450px;
+  width: 100%;
+  padding: 5rem calc((100vw - 1300px) / 2);
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const EmailContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    text-align: center;
+    margin-bottom: 1rem;
+    font-size: clamp(1rem, 5vw, 3rem);
+    padding: 0 1rem;
+  }
+
+  p {
+    text-align: center;
+    font-size: clamp(1rem, 2.5vw, 1rem);
+    padding: 0 1rem;
+    margin-bottom: 2rem;
+  }
+
+  form {
+    z-index: 10;
+  }
+`
+const FormWrap = styled.div`
+  input {
+    padding: 1rem 1.5rem;
+    outline: none;
+    width: 350px;
+    height: 48px;
+    border-radius: 50px;
+    border: none;
+    margin-right: 1rem;
+  }
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    padding: 0 1rem;
+
+    input {
+      margin-bottom: 1rem;
+      width: 100%;
+      margin-right: 0;
+    }
+  }
+`
 
 export default function Email() {
   return (
-    <div>
-      Hello world. I am the Email component.
-    </div>
+    <EmailContainer id="newsletter">
+      <EmailContent>
+        <h1>Submit Your Email</h1>
+        <p>Join my newsletter!</p>
+        <form action="#">
+          <FormWrap>
+            <label htmlFor="email">
+              <input 
+                type="email"
+                placeholder="Enter email"
+                id="email"
+              />
+            </label>
+            <Button
+              as="button"
+              type="submit"
+              primary="true"
+              round="true"
+              css={`
+                height: 48px;
+
+                @media screen and (max-width: 768px) {
+                  width: 100%;
+                  min-width: 350px;
+                }
+                
+                @media screen and (max-width: 400px) {
+                  width: 100%;
+                  min-width: 250px;
+                }
+              `}
+            >
+              Submit
+            </Button>
+          </FormWrap>
+        </form>
+      </EmailContent>
+    </EmailContainer>
   )
 };
