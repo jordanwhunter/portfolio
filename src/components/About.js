59bd95a27d0dc0f41e6fc5b31b76949093828ea9
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-04-09 15:20:12 -0400

    Removed border-radius

--- src/components/About.js
+++ src/components/About.js
@@ -169,7 +169,6 @@ export default function About() {
                 alignItems: "center",
                 justifyContent: "center",
                 justifyItems: "center",
-                borderRadius: "20px",
               }}
             />
           </Biography>
